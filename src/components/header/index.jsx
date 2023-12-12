ef59dfe169c98410b86c7672c9761e09daa40a4e
Kelvin Reverts <kelvinrev2@hotmail.com>
2023-12-12 19:19:32 -0300

    Initial commit

---
+++ src/components/header/index.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import  UserActionTypes from "../../redux/user/action-types"
+
+// Components
+import Cart from "../cart/index";
+
+// Styles
+import * as Styles from "./styles";
+
+function Header() {
+  const [cartIsVisible, setCartIsVisible] = useState(false);
+
+  const {currentUser} = useSelector((rootReducer) => rootReducer.userReducer)
+  const dispatch = useDispatch()
+  console.log({currentUser})
+  const handleCartClick = () => {
+    setCartIsVisible(true);
+  };
+
+  const handleLoginClick = ()=>{
+    dispatch({
+      type: UserActionTypes.LOGIN,
+      payload: {name:"Kelvin", email: "kelvin@email.com"}
+    })
+  }
+
+  const handleLogoutClick = ()=>{
+    dispatch({
+      type: UserActionTypes.LOGOUT,
+    })
+  }
+
+
+  return (
+    <Styles.Container>
+      <Styles.Logo>Redux Shopping</Styles.Logo>
+      <Styles.Buttons>
+      {currentUser ? <div onClick={handleLogoutClick}>Sair</div> :  <div onClick={handleLoginClick}>Login</div>}
+        <div onClick={handleCartClick}>Carrinho</div>
+      </Styles.Buttons>
+
+      <Cart isVisible={cartIsVisible} setIsVisible={setCartIsVisible} />
+    </Styles.Container>
+  );
+}
+
+export default Header;
---
+++ src/redux/user/reducer.js
@@ -0,0 +1,18 @@
+import  UserActionTypes from './action-types'
+
+const initialState = {
+  currentUser: null,
+};
+
+const userReducer = (state = initialState, action) => {
+  if (action.type === UserActionTypes.LOGIN) {
+    return { ...state, currentUser: action.payload};
+  }
+
+  if (action.type === UserActionTypes.LOGOUT) {
+    return { ...state, currentUser: null};
+  }
+  return state;
+};
+
+export default userReducer;
